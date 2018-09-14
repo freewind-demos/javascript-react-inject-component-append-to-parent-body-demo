963b443d7efe4208c2b7393411e68ec1ad8ae0c5
freewind <nowindlee@gmail.com>
2018-09-14 20:52:21 +0800

    demo

--- entry.jsx
+++ entry.jsx
@@ -1,13 +1,19 @@
 import Hello from './hello.jsx'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import $ from 'jquery'
 
-document.getElementById('trigger').onclick = function () {
-    ReactDOM.render(
-        <Hello/>,
-        window.parent.document.getElementById('main')
-    )
-}
+$(document).ready(function () {
+    $('#trigger').click(function () {
+        $(window.parent.document.body).append('<div id="for-inner-react">xxx</div>')
+
+        ReactDOM.render(
+            <Hello/>,
+            window.parent.document.getElementById('for-inner-react')
+        )
+    })
+})
+
 
 
 
